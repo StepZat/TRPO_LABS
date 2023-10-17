6e8b7f939eeec071cb3b09faf796d9700669b5b5
Stepan Zatinshchikov <step.zat@gmail.com>
2023-10-18 00:29:40 +0700

    Добавлено тестирование функционала класса CustomSet

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,77 @@
+import java.util.ArrayList;
 import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        CustomSet cset = new CustomSet(5, 6, 7, 7, 9);
-        System.out.println(cset);
-        cset.add(20);
-        System.out.println(cset);
-        cset.del(7);
-        System.out.println(cset);
+        // Создание пустого множества
+        CustomSet nullSet = new CustomSet();
+        System.out.print("Создание пустого множества: ");
+        System.out.println(nullSet);
 
-        CustomSet other = new CustomSet(7, 9, 10, 20);
-        CustomSet intersectionSet = cset.intersection(other);
-        System.out.println(intersectionSet);
-        CustomSet unionSet = cset.union(other);
+        // Создание множества на основе готового списка
+        List<Integer> readyList= new ArrayList<>();
+        readyList.add(1);
+        readyList.add(5);
+        readyList.add(7);
+        CustomSet readySet = new CustomSet(readyList);
+        System.out.print("Создание множества на основе готового списка: ");
+        System.out.println(readySet);
+
+        // Создание множества на основе параметров конструктора - целых чисел
+        CustomSet paramSet = new CustomSet(5, 6, 7, 7, 9);
+        System.out.print("Создание множества на основе параметров конструктора - целых чисел: ");
+        System.out.println(paramSet);
+
+        // Добавление элемента 20 в множество paramSet
+        System.out.print("Добавление элемента 20 в множество paramSet: ");
+        paramSet.add(20);
+        System.out.println(paramSet);
+
+        // Удаление элемента 6 из множества paramSet
+        System.out.print("Удаление элемента 6 из множества paramSet: ");
+        paramSet.del(6);
+        System.out.println(paramSet);
+
+        // Ввод элементов множества с клавиатуры
+        System.out.println("Ввод элементов множества с клавиатуры");
+        CustomSet inputSet = new CustomSet();
+        inputSet.addFromInput();
+        System.out.print("Множество из введенных данных: ");
+        System.out.println(inputSet);
+
+        // Операция объединения между readySet и paramSet
+        System.out.print("Операция объединения между readySet и paramSet: ");
+        CustomSet unionSet = readySet.union(paramSet);
         System.out.println(unionSet);
-        CustomSet diffSet = cset.diff(other);
+
+        // Операция пересечения между readySet и paramSet
+        System.out.print("Операция пересечения между readySet и paramSet: ");
+        CustomSet intersectionSet = readySet.intersection(paramSet);
+        System.out.println(intersectionSet);
+
+        // Операция разницы между readySet и paramSet
+        System.out.print("Операция разницы между readySet и paramSet: ");
+        CustomSet diffSet = readySet.diff(paramSet);
         System.out.println(diffSet);
-        CustomSet cset2 = new CustomSet();
-        cset2.readFromTextFile("file.txt");
-        cset2.add(123);
-        cset2.saveToBinaryFile("file2.bin");
-        CustomSet cset3 = new CustomSet();
-        cset3.readFromBinaryFile("file2.bin");
-        System.out.println(cset3);
+
+        // Сохранение объекта в тектовый и бинарный файл
+        inputSet.saveToTextFile("inputSet.txt");
+        System.out.println("Данные множества inputSet сохранены в текстовый файл inputSet.txt");
+        inputSet.saveToBinaryFile("inputSet.bin");
+        System.out.println("Данные множества inputSet сохранены в бинарный файл inputSet.bin");
+
+
+        // Загрузка нового множества из текстового и бинарного файлов
+        CustomSet readTextSet = new CustomSet();
+        readTextSet.readFromTextFile("inputSet.txt");
+        System.out.print("Множество readTextSet, загруженное из текстового файла inputSet.txt");
+        System.out.println(readTextSet);
+
+        CustomSet readBinarySet = new CustomSet();
+        readBinarySet.readFromBinaryFile("inputSet.bin");
+        System.out.print("Множество readBinarySet, загруженное из текстового файла inputSet.bin");
+        System.out.println(readBinarySet);
 
     }
 }
